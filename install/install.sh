6e2a1ad7cce5a4146d99e95bc6176cee2c419c9d
Leo <losoviz@gmail.com>
2019-08-03 23:16:44 +0530

    Validation in event "post-root-package-install"

--- install/install.sh
+++ install/install.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# Validate that all required environment variables are set
-./install/validate-env-variables.sh
-if [ $? -eq 0 ]
-then
-    # Fill wp-config.php
-    ./install/set-wp-config.sh
+# Fill wp-config.php
+./install/set-wp-config.sh
 
-    # Proceed to maybe install WordPress
-    ./install/maybe-install-wp.sh
-fi
+# Proceed to maybe install WordPress
+./install/maybe-install-wp.sh
