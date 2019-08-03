0d3bf8404fb102ed5afcfcdd2f4c269c0d4a7c0a
Leo <losoviz@gmail.com>
2019-08-03 10:27:46 +0530

    Made wget --quiet

--- install/install.sh
+++ install/install.sh
@@ -13,9 +13,9 @@ cd $FOLDER_NAME
 
 # Configure wp-config.php through WP-CLI
 #./set-wp-config.sh
-wget -O - https://raw.githubusercontent.com/leoloso/wp-install/master/install/set-wp-config.sh | bash
+wget -O - https://raw.githubusercontent.com/leoloso/wp-install/master/install/set-wp-config.sh --quiet | bash
 
 ## Check if WordPress is installed. If not, install it
 #./maybe-install-wp.sh
-wget -O - https://raw.githubusercontent.com/leoloso/wp-install/master/install/maybe-install-wp.sh | bash
+wget -O - https://raw.githubusercontent.com/leoloso/wp-install/master/install/maybe-install-wp.sh --quiet | bash
 
